1653020df077594869c2dcd336acca46f4ddf478
Andrea Jared <adj0803@gmail.com>
2023-10-31 16:22:32 -0400

    error in loading map

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,53 @@
-import data from '../data';
+import { useEffect, useReducer, useState } from 'react';
+// import data from '../data';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return {...state, products: action.payload, loading: false };
+        case 'FETCH_FAILURE':
+            return {...state, loading: false, error: action.payload };
+        default:
+        return state;
+    }
+}
 
 function HomeScreen () {
+    const [{ loading, error, products}, dispatch] = useReducer(logger(reducer), {   products: [],
+        loading: true, 
+        error: ''})
+    //const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+            } catch(err) {
+                dispatch({ type: 'FETCH_FAILURE', payload: err.message });
+            }
+            
+            //setProducts(result.data);
+        };
+        fetchData();
+    }, []);
+        
     return (
         <div>
         <h1>Featured Products</h1>
             <div className='products'>
-             {data.products.map(products => (
+                {loading ? (
+                    <div>Loading...</div>
+                ) : error ? (
+                    <div>{error}</div>
+                ) : (
+             products.map((products => (
                 <div className="product"key={products.slug}>
                   <Link to={`/product/${products.slug}`}>
                     <img src={products.image} alt={products.name} />
@@ -19,7 +60,7 @@ function HomeScreen () {
                 <button>Add to Cart</button>
               </div>
             </div>
-          ))}
+          ))
         </div>
         </div>
     )
