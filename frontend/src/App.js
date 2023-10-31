9eb0cdc3fea4b73038b3954125b03c74fe95d4fa
Andrea Jared <adj0803@gmail.com>
2023-10-31 12:59:13 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <a href="/"> J.E.M.Z </a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
