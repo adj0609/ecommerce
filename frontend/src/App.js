a13a00ffce11a1b0c855292da948e8b8cefd02d9
Andrea Jared <adj0803@gmail.com>
2023-11-02 11:27:13 -0400

    product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
-import './App.css';
-import { BrowserRouter, Link,  Route, Routes} from'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
-
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-       <Link to="/"> J.E.M.Z </Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>} />
-          <Route path="/" element={<HomeScreen />} />
-          </Routes>   
-      </main>
-    </div>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
